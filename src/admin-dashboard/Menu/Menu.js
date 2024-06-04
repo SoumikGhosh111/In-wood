ec986240f4effde96e7cac307a295a4c7ec40515
SoumikGhosh111 <soumikbuie2001@gmail.com>
2024-06-04 14:04:09 +0530

    implemented update menu functionality

--- src/admin-dashboard/Menu/Menu.js
+++ src/admin-dashboard/Menu/Menu.js
@@ -3,20 +3,28 @@ import "./Menu.css";
 import DoneRoundedIcon from '@mui/icons-material/DoneRounded';
 import ClearRoundedIcon from '@mui/icons-material/ClearRounded';
 import { baseUrl } from '../../functions/baseUrl';
-
 import axios from 'axios';
-
+import Backdrop from '@mui/material/Backdrop';
+import BorderColorRoundedIcon from '@mui/icons-material/BorderColorRounded';
 //toast
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-
-
 
 function Menu() {
   const [menu, setMenu] = useState(null);
   const [editItemId, setEditItemId] = useState(null); // Track which item is being edited
   const [fileInputVisible, setFileInputVisible] = useState(false);
   const [selectedFile, setSelectedFile] = useState(null);
+  const [productDetails, setProductDetails] = useState({
+    img: '',
+    title: '',
+    desc: '',
+    catagory: '',
+    prices: [],
+    extraOptions: [{ text: '', price: '' }]
+  });
+  const [open, setOpen] = useState(false);
+  const [uploadedImageUrl, setUploadedImageUrl] = useState('')
 
   const fetchMenu = async () => {
     try {
@@ -32,102 +40,153 @@ function Menu() {
     fetchMenu();
   }, []);
 
-  // const handleImageUpload = async (itemId, file) => {
-  //   try {
-  //     // Upload image to Cloudinary
-  //     const cloudinaryUrl = 'CLOUDINARY_UPLOAD_URL_HERE';
-  //     const formData = new FormData();
-  //     formData.append('file', file);
-  //     formData.append('upload_preset', 'YOUR_CLOUDINARY_UPLOAD_PRESET');
+  const handleEditButtonClick = (item) => {
+    setEditItemId(item._id);
+    setProductDetails({
+      img: item.img,
+      title: item.title,
+      desc: item.desc,
+      catagory: item.catagory,
+      prices: item.prices,
+      extraOptions: item.extraOptions
+    });
+  }
 
-  //     const response = await fetch(cloudinaryUrl, {
-  //       method: 'POST',
-  //       body: formData
-  //     });
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setProductDetails({
+      ...productDetails,
+      [name]: value
+    });
+  }
 
-  //     if (!response.ok) {
-  //       throw new Error('Failed to upload image to Cloudinary');
-  //     }
+  const handleCatagoryChange = (e) => {
+    const { value } = e.target;
+    setProductDetails(prevState => ({
+     ...prevState,
+      catagory: value
+    }));
+  };
+  
 
-  //     const data = await response.json();
-  //     const imageUrl = data.secure_url;
+  // handle prices array 
+  const handlePriceChange = (index, value) => {
+    const newPrices = [...productDetails.prices];
+    newPrices[index] = value;
+    setProductDetails({
+      ...productDetails,
+      prices: newPrices
+    });
+  }
 
-  //     // Now make an API call to update the image URL for the specific item
-  //     const updateImageUrlApiUrl = `http://localhost:8000/api/product/updateImage/${itemId}`;
-  //     const updateResponse = await fetch(updateImageUrlApiUrl, {
-  //       method: 'PUT',
-  //       headers: {
-  //         'Content-Type': 'application/json'
-  //       },
-  //       body: JSON.stringify({ imageUrl })
-  //     });
-
-  //     if (!updateResponse.ok) {
-  //       throw new Error('Failed to update image URL');
-  //     }
-
-  //     // If successful, fetch the updated menu
-  //     fetchMenu();
-  //     setEditItemId(null); // Reset editItemId after successful update
-  //     setFileInputVisible(false); // Hide file input after upload
-  //     setSelectedFile(null); // Reset selected file
-  //   } catch (error) {
-  //     console.error("Error updating image URL:", error);
-  //   }
-  // }
+  // const handlePriceChange = (index, value) => {
+  //   // converting the number from text to number
+  //   const newValue = Number(value);
+  
+  //   const newPrices = [...productDetails.prices];
+  //   newPrices[index] = newValue;
+  
+  //   setProductDetails({
+  //    ...productDetails,
+  //     prices: newPrices
+  //   });
+  // };
 
 
 
-  const handleSubmitImage = () => {
-    if (selectedFile && editItemId) {
-      handleImageUpload(editItemId, selectedFile);
-    } else {
-      console.error("No file selected or item ID missing.");
+  // handle extra toppings array
+  const handleExtraOptionChange = (index, type, value) => {
+    // creating a deep copy
+    const newOptions = productDetails.extraOptions.map(option => ({ ...option }));
+
+    if (type === 'text') {
+      newOptions[index].text = value;
+    } else if (type === 'price') {
+      newOptions[index].price = value;
     }
+
+    setProductDetails({
+      ...productDetails,
+      extraOptions: newOptions
+    });
+  };
+
+  const handleAddExtraOption = () => {
+    setProductDetails({
+      ...productDetails,
+      extraOptions: [...productDetails.extraOptions, '']
+    });
+  }
+
+  const handleRemoveExtraOption = (index) => {
+    const newOptions = productDetails.extraOptions.filter((_, i) => i !== index);
+    setProductDetails({
+      ...productDetails,
+      extraOptions: newOptions
+    });
   }
 
 
-  const handleImageUpload = async (id, file) => {
+  const handleImageUpload = async (e) => {
+    const file = e.target.files[0]; // Get the first selected file
+    // setImage(file);
+
     const formData = new FormData();
     formData.append("image", file);
 
     try {
-      const email = localStorage.getItem('userEmail'); 
-      const token = localStorage.getItem('token'); 
       const { data } = await axios.post(`${baseUrl}/api/image/uploadImage`, formData);
 
+      setUploadedImageUrl(data.url);
 
-      const imageData = {
-        img: data.url,
-        id: id
-      }
-
-      const response = await fetch(`${baseUrl}/api/product/productImage/${email}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json', 
-          'Authorization': `Bearer ${token}`
-        },
-        body: JSON.stringify(imageData)
-      })
-      const result = await response.json();  
-
-      console.log(data.url, "new URL");
-      console.log(result, "imageData");
-
-      fetchMenu(); 
+      productDetails.img = data.url; 
       // console.log(image); 
-      toast.success(`Image Uploaded Success fully `);
-      setEditItemId(null);
-      setFileInputVisible(false);
-      setSelectedFile(null);
     }
     catch (err) {
       console.log(err);
-      toast.error("Unable to upload image")
+    }
+  };
+
+  const handleUpdateProduct = async () => {
+    try {
+      const token = localStorage.getItem('token')
+      const email = localStorage.getItem('userEmail'); 
+      const response = await fetch(`${baseUrl}/api/product/productDetails/${email}`, {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({ id: editItemId, ...productDetails })
+      });
+
+      const result = await response.json();
+      console.log(result);
+      if (result.success) {
+        toast.success("Product details updated successfully");
+        setOpen(false); 
+        fetchMenu();
+        setEditItemId(null);
+      } else {
+        toast.error("Failed to update product details");
+      }
+    } catch (error) {
+      console.error("Error updating product details:", error);
+      toast.error(error.message);
     }
 
+    
   }
+
+  // const handleSubmitImage = () => {
+  //   if (selectedFile && editItemId) {
+  //     handleImageUpload(editItemId, selectedFile);
+  //   } else {
+  //     console.error("No file selected or item ID missing.");
+  //   }
+  // }
+
+  
 
   return (
     <div className='menu-wrapper'>
@@ -141,6 +200,9 @@ function Menu() {
               <th>Name</th>
               <th>Description</th>
               <th>Extra Toppings</th>
+              <th>Category</th>
+              <th>Prices</th>
+              <th>Modify Menu</th>
             </tr>
           </thead>
           <tbody>
@@ -153,48 +215,136 @@ function Menu() {
                       <div className='admin-menu-img-img'>
                         <img
                           src={item.img}
-                          style={{ cursor: "pointer" }}
-                          alt= {`..............................................
-                          upload image`}
-                          onClick={() => {
-                            setEditItemId(item._id);
-                            setFileInputVisible(true);
-                          }}
+                          alt="No Img"
                         />
                       </div>
-                      {editItemId === item._id && fileInputVisible && (
-                        <div className='menu-order-input-butttons'>
-                          <input
-                            type="file"
-                            onChange={(e) => setSelectedFile(e.target.files[0])}
-                          />
-                          <button onClick={handleSubmitImage}><DoneRoundedIcon sx={{ transform: "translateY(10%)" }} /></button>
-                          <button onClick={() => setFileInputVisible(false)}><ClearRoundedIcon sx={{ transform: "translateY(10%)" }} /></button>
-                        </div>
-                      )}
                     </div>
                   </td>
                   <td>{item.title}</td>
                   <td>{item.desc}</td>
                   <td>
                     {item.extraOptions?.map((topping, index) => (
-                      <span key={index}>{topping.text}<br /></span>
+                      <span key={index}>{topping.text} &nbsp; ${topping.price}<br /></span>
                     ))}
                   </td>
+                  <td>{item.catagory}</td>
+                  <td>{item.prices.map((price) => (<div key={price}>$&nbsp; {price}</div>))}</td>
+                  <td><button onClick={() => { handleEditButtonClick(item); setOpen(true) }} style={{border: 'none', background: 'transparent', cursor: 'pointer'}}><BorderColorRoundedIcon /></button></td>
                 </tr>
               )) :
               <tr>
-                <td colSpan="5">Loading...</td>
+                <td colSpan="8">Loading...</td>
               </tr>
             }
           </tbody>
         </table>
       </div>
+
+      <Backdrop
+        sx={{ color: '#000', zIndex: (theme) => theme.zIndex.drawer + 1, backgroundColor: "rgba(0, 0, 0, 0.238)" }}
+        open={open}
+      >
+
+        {editItemId && (
+          <div className="edit-form">
+            <div className='edit-form-header'>
+              <h2 style={{marginLeft: '1rem'}}>Edit Product</h2>
+              <div className='cancel-bttn' onClick={() => setOpen(false)}>
+                <ClearRoundedIcon sx={{ fontSize: "30px" }} />
+              </div>
+            </div>
+            <div style={{ padding: '20px 20px' }}>
+              <div className="form-group">
+                <label>Title:</label>
+                <input type="text" name="title" value={productDetails.title} onChange={handleInputChange} />
+              </div>
+              <div className="form-group">
+                <label>Description:</label>
+                <input type="text" name="desc" value={productDetails.desc} onChange={handleInputChange} />
+              </div>
+              <div className="form-group">
+                <label>Category:</label>
+                <select name="category" value={productDetails.catagory} onChange={(e) => handleCatagoryChange(e)}>
+                  {/* <option value="">Select a category</option> */}
+                  <option value="Pizza large 18inch">Pizza large 18inch</option>
+                  <option value="Pizza by Slice">Pizza by Slice</option>
+                  <option value="Inwood Favorites">Inwood Favorites</option>
+                  <option value="Calzones">Calzones</option>
+                  <option value="Family Special">Family Special</option>
+                  <option value="Everyday Special">Everyday Special</option>
+                  <option value="Ice Cream">Ice Cream</option>
+                  <option value="Milk Shake">Milk Shake</option>
+                  <option value="Drinks">Drinks</option>
+                </select>
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="img">Image URL:</label>
+                <input
+                  type="text"
+                  id="img"
+                  name="img"
+                  value={productDetails.img}
+
+                  disabled
+                />
+                <br /><br />
+
+                <label htmlFor="uploadImage">Upload Image:</label>
+                <input
+                  type="file"
+                  id="uploadImage"
+                  accept="image/"
+                  onChange={(e) => handleImageUpload(e)}
+                />
+              </div>
+
+              <div className="form-group">
+                <label>Prices:</label>
+                {productDetails.prices.map((price, index) => (
+                  <div key={index} className="price-input">
+                    <input
+                      type="number"
+                      value={price}
+                      onChange={(e) => handlePriceChange(index, e.target.value)}
+                    />
+                  </div>
+                ))}
+              </div>
+              <div className="form-group">
+                <label>Extra Options:</label>
+                {productDetails.extraOptions.map((option, index) => (
+                  <div key={index} className="option-input">
+                    <input
+                      type="text"
+                      placeholder="Option Text"
+                      value={option.text}
+                      onChange={(e) => handleExtraOptionChange(index, 'text', e.target.value)}
+                    />
+                    <input
+                      type="number"
+                      placeholder="Option Price"
+                      value={option.price}
+                      onChange={(e) => handleExtraOptionChange(index, 'price', e.target.value)}
+                    />
+                    <button type="button" onClick={() => handleRemoveExtraOption(index)}>Remove</button>
+                  </div>
+                ))}
+                <button className='add-toppings' type="button" onClick={handleAddExtraOption}>Add Option</button>
+              </div>
+              <div className="form-actions">
+                <button className="update-btn" onClick={handleUpdateProduct}>Update</button>
+                <button className="cancel-btn" onClick={() => { setEditItemId(null); setOpen(false) }}>Cancel</button>
+              </div>
+            </div>
+          </div>
+        )}
+
+      </Backdrop>
       <ToastContainer />
     </div>
   );
 }
 
 export default Menu;
-
 
